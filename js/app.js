7e47742dc5282679a3f36a59311d2340d4448adc
Ling Ding <azusakaworu@gmail.com>
2018-10-04 19:13:59 -0400

    updats gulp-sass serve goupfile.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,30 @@
 $(document).foundation()
 
 // write your own JS here:
-var lBox = document.querySelector('#imgS');
 
-function abc(){
 
-       lBox.style.width="200%";
+
+
+var mask = document.getElementsByClassName('mask')[0]; 
+var imgBox = document.getElementsByClassName('imgBox')[0]; 
+var dataTitle = document.getElementsByClassName('data-title')[0]; 
+var video = document.getElementsByTagName('video')[0]; 
+var a = document.getElementsByClassName('-webkit-media-controls-fullscreen-button')[0];
+
+imgBox.onclick = function () { 
+    imgBox.classList.add('img_current'); 
+    mask.style.display = 'block'; 
+    dataTitle.style.display = 'block'; 
 }
 
-lBox.addEventListener('click',abc);
+video.onclick = function () { 
+    video.setAttribute('class', 'current'); 
+    mask.style.display = 'block'; 
+}
+
+mask.onclick = function () { 
+    mask.style.display = 'none'; 
+    imgBox.classList.remove('img_current');
+    video.classList.remove('current'); 
+    dataTitle.style.display = 'none'; 
+}
